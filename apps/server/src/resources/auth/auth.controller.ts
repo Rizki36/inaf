dcdd41399cbe6f753343393863a8abba9b054e58
Rizki36 <cahrizki.rf@gmail.com>
2022-02-12 22:12:08 +0700

    fix(server): fixed short token age

--- apps/server/src/resources/auth/auth.controller.ts
+++ apps/server/src/resources/auth/auth.controller.ts
@@ -1,13 +1,17 @@
-import { successResponse } from './../../helpers/methods';
+import { successResponse } from "./../../helpers/methods";
 import { NextFunction, Request, Response } from "express";
-import { accountService, signinService, signupService } from './auth.service'
-import { StatusCodes } from 'http-status-codes';
-import { ErrorUserNotFound } from '../../lib/Errors';
+import { accountService, signinService, signupService } from "./auth.service";
+import { StatusCodes } from "http-status-codes";
+import { ErrorUserNotFound } from "../../lib/Errors";
 
 // signup
-export const signup = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+export const signup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<any> => {
     // get data from body
-    const { email, password, username, name } = req.body
+    const { email, password, username, name } = req.body;
 
     try {
         // get data
@@ -15,63 +19,74 @@ export const signup = async (req: Request, res: Response, next: NextFunction): P
             email,
             password,
             username,
-            name
-        })
+            name,
+        });
 
-        res.status(StatusCodes.CREATED)
-            .send(successResponse({ data }))
+        res.status(StatusCodes.CREATED).send(successResponse({ data }));
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
-// signin 
-export const signin = async (req: Request, res: Response, next: NextFunction) => {
+// signin
+export const signin = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     // get data from body
-    const { username, password } = req.body
+    const { username, password } = req.body;
 
     try {
         // get data
         const data = await signinService({
             username,
-            password
-        })
+            password,
+        });
 
         //responding to client request with user profile success message and  access token .
-        res.cookie('token', data.accessToken, {
-            maxAge: Number(100) * 1000,
+        res.cookie("token", data.accessToken, {
+            maxAge: 1000 * 60 * 60,
             httpOnly: true,
-            path: '/',
+            path: "/",
             // secure: true
-        }).json(successResponse({ data }))
+        }).json(successResponse({ data }));
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
 // get account data
-export const account = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+export const account = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<any> => {
     try {
-        if (!req?.user) throw new ErrorUserNotFound()
+        if (!req?.user) throw new ErrorUserNotFound();
 
-        const account = await accountService(req.user?.id)
+        const account = await accountService(req.user?.id);
 
-        res.send(successResponse({ data: account }))
+        res.send(successResponse({ data: account }));
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
-export const logout = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+export const logout = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<any> => {
     try {
         console.log(req);
-        res.cookie('token', '', {
+        res.cookie("token", "", {
             maxAge: Number(100) * 1000,
             httpOnly: true,
-            path: '/',
+            path: "/",
             // secure: true
-        }).json(successResponse({}))
+        }).json(successResponse({}));
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
