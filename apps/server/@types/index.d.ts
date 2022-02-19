b55de19b8e814dbc4312ac9166489b5df4b32784
Rizki36 <cahrizki.rf@gmail.com>
2022-02-19 11:33:22 +0700

    feat(server): add AsyncReturnType

--- apps/server/@types/index.d.ts
+++ apps/server/@types/index.d.ts
@@ -7,14 +7,14 @@ export interface Errors {
 
 export interface UserLogin {}
 
-export interface IResponse {
+export interface IResponse<T = Record<string, any>, P = Record<string, any>> {
     status: boolean;
     message: string;
-    data: Record<string, any> | null;
-    errors: Record<string, any> | null;
+    data: T | null;
+    errors: P | null;
 }
 
-export interface SortProps<T>{
+export interface SortProps<T> {
     field: keyof T;
     sort: "asc" | "desc";
 }
@@ -22,6 +22,14 @@ export interface SortProps<T>{
 export interface PaginationProps<T> {
     page: number;
     perPage: number;
-    sortPage?: SortProps<T>
+    sortPage?: SortProps<T>;
     search?: string;
 }
+
+export type AsyncReturnType<T extends (...args: any) => any> = T extends (
+    ...args: any
+) => Promise<infer U>
+    ? U
+    : T extends (...args: any) => infer U
+    ? U
+    : any;
