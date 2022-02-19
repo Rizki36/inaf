3d4afa1ae14a9722055e32552b2f1f5b7a207622
Rizki36 <cahrizki.rf@gmail.com>
2022-02-19 11:32:46 +0700

    refactor(server): add type successResponse

--- apps/server/src/helpers/methods.ts
+++ apps/server/src/helpers/methods.ts
@@ -1,63 +1,75 @@
-import { IResponse } from './../../@types/index.d';
+import { IResponse } from "./../../@types/index.d";
 
-type data = Record<string, any>
+type data = Record<string, any>;
 
-export const successResponse = ({ message, data }: { message?: string, data?: Record<string, any> }): IResponse => {
-	return {
-		status: true,
-		message: message ?? 'Success !',
-		data: data ?? null,
-		errors: null
-	}
-}
+export const successResponse = <T = Record<string, any> | null>({
+    message,
+    data,
+}: {
+    message?: string;
+    data?: T;
+}): IResponse<T> => {
+    return {
+        status: true,
+        message: message ?? "Success !",
+        data: data ?? null,
+        errors: null,
+    };
+};
 
-export const errorResponse = ({ message, errors }: { message?: string, errors?: Record<string, any> }): IResponse => {
-	return {
-		status: false,
-		message: message ?? 'Error !',
-		data: null,
-		errors: errors ?? null
-	}
-}
+export const errorResponse = ({
+    message,
+    errors,
+}: {
+    message?: string;
+    errors?: Record<string, any>;
+}): IResponse => {
+    return {
+        status: false,
+        message: message ?? "Error !",
+        data: null,
+        errors: errors ?? null,
+    };
+};
 
 export const createdResponse = (data?: data) => {
-	return successResponse({
-		message: 'Created !',
-		data
-	})
-}
+    return successResponse({
+        message: "Created !",
+        data,
+    });
+};
 
 export const notFountResponse = (errors?: data) => {
-	return errorResponse({
-		message: 'Not Found !',
-		errors
-	})
-}
+    return errorResponse({
+        message: "Not Found !",
+        errors,
+    });
+};
 
 export const unauthorizedResponse = (errors?: data) => {
-	return errorResponse({
-		message: 'Unauthorized !',
-		errors
-	})
-}
+    return errorResponse({
+        message: "Unauthorized !",
+        errors,
+    });
+};
 
 export const unauthenticatedResponse = (errors?: data) => {
-	return errorResponse({
-		message: 'Unauthenticated!',
-		errors
-	})
-}
+    return errorResponse({
+        message: "Unauthenticated!",
+        errors,
+    });
+};
 
 export const errorValidationResponse = (errors?: data) => {
-	return errorResponse({
-		message: 'Data is not valid!',
-		errors
-	})
-}
+    return errorResponse({
+        message: "Data is not valid!",
+        errors,
+    });
+};
 
 export const invalidPasswordResponse = (errors?: data) => {
-	return errorResponse({
-		message: 'Password is not valid!',
-		errors
-	})
-}
+    return errorResponse({
+        message: "Password is not valid!",
+        errors,
+    });
+};
