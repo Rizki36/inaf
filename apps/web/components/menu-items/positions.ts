d0d5a96488f6f1fd00c926bf0074e9c2fce9a0ee
Rizki36 <cahrizki.rf@gmail.com>
2022-02-26 16:07:06 +0700

    fix(web): missing menu positions

---
+++ apps/web/components/menu-items/positions.ts
@@ -0,0 +1,20 @@
+import { IconDashboard, IconStack } from "@tabler/icons";
+const icons = { IconDashboard, IconStack };
+
+const users = {
+    id: "positions",
+    title: "Positions",
+    type: "group",
+    children: [
+        {
+            id: "default",
+            title: "Positions",
+            type: "item",
+            url: "/positions",
+            icon: icons.IconStack,
+            breadcrumbs: false,
+        },
+    ],
+};
+
+export default users;
