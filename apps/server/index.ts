b012d372ec0470d5d78753f29f129151b7659ed3
Rizki36 <cahrizki.rf@gmail.com>
2022-03-15 21:05:27 +0700

    fix(server): fixed error cause isolatedModules

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -2,5 +2,7 @@ export * from "./src/resources/user/user.dto";
 // export * from "./src/resources/user/user.validation";
 export * from "./src/resources/position/position.dto";
 
-import { User, Position } from "@prisma/client";
-export { User, Position };
+import { User as IUser, Position as IPosition } from "@prisma/client";
+
+export type User = IUser;
+export type Position = IPosition;
