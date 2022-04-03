a50ef7bc0a33f5073a0d1f54a6a016b816874446
Rizki36 <cahrizki.rf@gmail.com>
2022-04-03 16:29:51 +0700

    feat: export IResponse and ITeam

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,9 +1,18 @@
 export * from "./src/resources/user/user.dto";
 export * from "./src/resources/position/position.dto";
 export * from "./src/resources/project/project.dto";
+export * from "./src/resources/team/team.dto";
 
-import { User as IUser, Position as IPosition,Project as IProject } from "@prisma/client";
+import {
+    User as IUser,
+    Position as IPosition,
+    Project as IProject,
+    Team as ITeam,
+} from "@prisma/client";
+import { IResponse as Response } from "./@types/index.d";
+export type IResponse<T> = Response<T>;
 
 export type User = IUser;
 export type Position = IPosition;
 export type Project = IProject;
+export type Team = ITeam;
