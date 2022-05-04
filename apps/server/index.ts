bb1bb64a13588e5a21cbae773d95903ff6d21fd8
Rizki36 <cahrizki.rf@gmail.com>
2022-05-04 22:14:39 +0700

    feat: export Task Type

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -5,6 +5,7 @@ export * from "./src/resources/team/team.dto";
 export * from "./src/resources/task_group/task_group.dto";
 export * from "./src/resources/task/task.dto";
 export * from "./src/resources/task_worker/task_worker.dto";
+export * from "./src/resources/dashboard/dashboard.dto";
 
 import {
     User as IUser,
@@ -12,6 +13,7 @@ import {
     Project as IProject,
     Team as ITeam,
     TaskWorker as ITaskWorker,
+    Task as ITask,
 } from "@prisma/client";
 import { IResponse as Response } from "./@types/index.d";
 export type IResponse<T> = Response<T>;
@@ -20,6 +22,7 @@ export type User = IUser;
 export type Position = IPosition;
 export type Project = IProject;
 export type Team = ITeam;
+export type Task = ITask;
 export type TeamWorker = ITaskWorker;
 
 // TODO : move into types folder
